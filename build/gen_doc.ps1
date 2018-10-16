6738604af76abd76661ee20d063070a9e95d46d9
Christopher Seidl <christopher.seidl@arm.com>
2018-10-15 17:01:02 -0700

    Build the documentation using Windows PowerShell.

---
+++ build/gen_doc.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+    This script builds the documentation in teh Embedded C GitHub repository.
+
+.DESCRIPTION
+    Use this script for generating HTML documentation and EPUB from the markdown files.
+
+.NOTES
+    Author:  Christopher Seidl
+    Date:    October 15, 2018
+#>
+Write-Output "Switching directory to ..\chapters"
+Push-Location ..\chapters
+Write-Output "Removing previous builds"
+Remove-Item .\all_chapters.md -ErrorAction SilentlyContinue
+Remove-Item .\embedded_c.html -ErrorAction SilentlyContinue
+Remove-Item .\embedded_c.epub -ErrorAction SilentlyContinue
+Write-Output "Concatenating markdown files"
+Get-Content .\*.md | Set-Content all_chapters.md
+Write-Output "Generating HTML documentation"
+&pandoc.exe -f markdown -t html -o "embedded_c.html" --reference-links -s -S all_chapters.md
+Write-Output "Generating EPUB"
+&pandoc.exe -f markdown -t epub -o "embedded_c.epub" --reference-links -s -S metadata.yaml all_chapters.md
+Write-Output "Going back to build directory and done!"
+Pop-Location
