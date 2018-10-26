2738ce73ff90b5f1ae6f63fe498211d83bf864aa
Christopher Seidl <christopher.seidl@arm.com>
2018-10-26 13:03:54 +0200

    More work on actual content.

---
+++ projects/lab03/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>              // Standard I/O - required for printf() function
+
+// Constants
+#define   CONST1 0x42
+const int CONST2 = 0xAB;
+
+// Variables
+int myVar1 = CONST1;
+int myVar2;
+int n = 5, m = 0, res;
+
+// main() function
+int main(void) {
+	if (n) {
+		printf("n is TRUE\n");
+	}
+	if (m == 1) {
+		printf("m is FALSE\n");
+	}
+  // Endless loop
+	while(1);
+}
