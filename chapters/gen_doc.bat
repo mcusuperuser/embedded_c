c59d6e2c22654dbd1b66d8d7c8f315866d1f7111
mcusuperuser <mcusuperuser@users.noreply.github.com>
2020-03-14 22:18:07 +0100

    Changed output formats and styling

--- chapters/gen_doc.bat
+++ chapters/gen_doc.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 ECHO Generating Embedded C EPUB
 
-"%ProgramFiles%\Pandoc\pandoc.exe" -f markdown -t epub --toc -o embedded_c.epub --reference-links -s metadata.yaml complete.md
+"%ProgramFiles%\Pandoc\pandoc.exe" -f markdown -t epub --toc --css=pandoc.css -o embedded_c.epub --reference-links -s metadata.yaml complete.md
+"%ProgramFiles%\Pandoc\pandoc.exe" -f markdown -t html --toc --css=pandoc.css -o embedded_c.html --reference-links -s metadata.yaml complete.md
