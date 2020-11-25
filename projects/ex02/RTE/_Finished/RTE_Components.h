05eaf40d6740ec7b26e793af753e4ed13b55363d
mcusuperuser <mcusuperuser@users.noreply.github.com>
2020-11-25 20:44:28 +0100

    Working on ex02

--- projects/ex02/RTE/_Finished/RTE_Components.h
+++ projects/ex02/RTE/_Finished/RTE_Components.h
@@ -16,9 +16,6 @@
  */
 #define CMSIS_device_header "ARMCM3.h"
 
-/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
-#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
-          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
 
 
 #endif /* RTE_COMPONENTS_H */
